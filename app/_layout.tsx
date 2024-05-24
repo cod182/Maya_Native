c428d34e3fe8ade2cba87eb3fabb357c96828e2e
Codie Stephens-Evans <codie18@gmail.com>
2024-05-24 16:40:49 +0100

    started home page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -37,12 +37,3 @@ const RootLayout = () => {
 }
 
 export default RootLayout
-
-const styles = StyleSheet.create({
-  container: {
-    display: 'flex',
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-})
---
+++ components/CustomButton.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+
+import React from 'react'
+
+const CustomButton = () => {
+  return (
+    <View>
+      <Text>CustomButton</Text>
+    </View>
+  )
+}
+
+export default CustomButton
