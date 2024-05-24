cc770926b213dc5d1b32a49e480b15c1a835859f
Codie Stephens-Evans <codie18@gmail.com>
2024-05-24 16:51:52 +0100

    home page created

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,7 @@
 import { Image, ScrollView, Text, View } from 'react-native';
+import { Redirect, router } from 'expo-router';
 
 import CustomButton from '@/components/CustomButton';
-import { Link } from 'expo-router';
-import { PropsWithChildren } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar'
 import { images } from '../constants'
@@ -14,7 +13,7 @@ export default function App() {
         contentContainerStyle={{ height: "100%" }}
       >
         <View
-          className='w-full justify-start items-center h-full px-4'
+          className='w-full justify-center items-center min-h-[85vh] px-4'
         >
           <Image source={images.logo} className='w-[130px] h-[84px]' resizeMode='contain' />
           <Image source={images.cards} className='max-w-[380px] w-full h-[300px]' resizeMode='contain' />
@@ -30,9 +29,10 @@ export default function App() {
           <Text className='text-sm font-pregular text-gray-100 mt-7 text-center'>
             Where creativity meets innovation: embark on a journey of limitless exploration with Maya
           </Text>
-          <CustomButton />
+          <CustomButton title='Continue with Email' handlePress={() => { router.push('/sign-in') }} containerStyles='w-full mt-7' />
         </View>
       </ScrollView>
+      <StatusBar backgroundColor='#161622' style='light' />
     </SafeAreaView>
   )
 }
--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,12 +1,25 @@
-import { Text, View } from 'react-native'
+import { Text, TouchableOpacity } from 'react-native'
 
 import React from 'react'
 
-const CustomButton = () => {
+type Props = {
+  title: string;
+  handlePress: () => void;
+  containerStyles: string;
+  textStyles?: string;
+  isLoading?: boolean;
+}
+
+const CustomButton = ({ title, handlePress, containerStyles, textStyles, isLoading }: Props) => {
   return (
-    <View>
-      <Text>CustomButton</Text>
-    </View>
+    <TouchableOpacity
+      onPress={handlePress}
+      activeOpacity={0.7}
+      className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${isLoading ? 'opacity-50' : ''}`}
+      disabled={isLoading}
+    >
+      <Text className={`text-primary font-psemibold text-lg ${textStyles}`}>{title}</Text>
+    </TouchableOpacity>
   )
 }
 
