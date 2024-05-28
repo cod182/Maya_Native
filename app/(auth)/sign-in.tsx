67dd6e9ebc7bc1a38f07eaa53c82f422d377fff4
Codie Stephens-Evans <codie18@gmail.com>
2024-05-28 14:24:34 +0100

    added a sign up link

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,13 +1,19 @@
 import { Image, ScrollView, Text, View } from 'react-native'
 import React, { useState } from 'react'
 
+import CustomButton from '@/components/CustomButton';
 import FormField from '@/components/FormField';
+import { Link } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '../../constants'
 
 const SignIn = () => {
 
   const [form, setForm] = useState({ email: '', password: '', })
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const submit = () => {
+
+  }
   return (
     <SafeAreaView className='h-full bg-primary'>
       <ScrollView>
@@ -28,6 +34,23 @@ const SignIn = () => {
             handleChangeText={(e: string) => setForm({ ...form, password: e })}
             otherStyles='mt-7'
           />
+
+          <CustomButton
+            title='Sign In'
+            handlePress={submit}
+            containerStyles='mt-7'
+            isLoading={isSubmitting}
+          />
+
+          <View className='flex-row justify-center gap-2 pt-5'>
+            <Text className='text-lg text-gray-100 font-pregular'>
+              Don't Have An Account?
+            </Text>
+            <Link href='sign-up' className='text-lg font-psemibold text-secondary-100'>
+              Sign Up
+            </Link>
+          </View>
+
         </View>
       </ScrollView>
     </ SafeAreaView >
